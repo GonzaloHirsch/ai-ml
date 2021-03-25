72259b08cbf78b7f5d1c910433baa322b4f950f1
Gonzalo Hirsch <hirschgonzalo@gmail.com>
2021-03-24 22:12:47 -0300

    Improved mutation file

--- TP2/methods/mutacion.py
+++ TP2/methods/mutacion.py
@@ -3,43 +3,76 @@ import random
 # Local imports
 from items import Items
 from config import Config
-from constants import ItemTypes
+from constants import ItemTypes, MutacionOptions
 
-def mutacionCompleta(ch):
-    # Recover instances
-    items = Items.getInstance()
-    conf = Config.getInstance()
-    # Generate random number
-    rnd = random.uniform(0, 1)
-    # If less than probability, mutate
-    if rnd <= conf.pm:
-        # Iterate item types and get all new items
-        for item in ItemTypes:
-            # Get new random item for that type
+class Mutacion:
+    def __init__(self, mut):
+        self.mut = mut
+        self.mutacion = self.mutaciones[mut]
+
+    # -----------------------------------------------------------------
+    # MUTACION FUNCTIONS
+    # -----------------------------------------------------------------
+
+    def __mutacionCompleta(ch):
+        # Recover instances
+        items = Items.getInstance()
+        conf = Config.getInstance()
+        # Generate random number
+        rnd = random.uniform(0, 1)
+        # If less than probability, mutate
+        if rnd <= conf.pm:
+            # Iterate item types and get all new items
+            for item in ItemTypes:
+                # Get new random item for that type
+                if item != ItemTypes.ALTURA:
+                    newItem = items.getRandomItem(item)
+                else:
+                    newItem = items.getRandomHeight()
+                # Set the gene in the character
+                ch.setGene(item, newItem)
+        return ch
+
+    def __mutacionGen(ch):
+        # Recover instances
+        items = Items.getInstance()
+        conf = Config.getInstance()
+        # Generate random number
+        rnd = random.uniform(0, 1)
+        # If less than probability, mutate
+        if rnd <= conf.pm:
+            # Generate a random property to alter
+            rnd = int(random.uniform(0, len(ItemTypes)))
+            # Get item for that index
+            item = ItemTypes(rnd)
+            # Generate random item
             if item != ItemTypes.ALTURA:
                 newItem = items.getRandomItem(item)
             else:
                 newItem = items.getRandomHeight()
             # Set the gene in the character
             ch.setGene(item, newItem)
+        return ch
 
-def mutacionGen(ch):
-    # Recover instances
-    items = Items.getInstance()
-    conf = Config.getInstance()
-    # Generate random number
-    rnd = random.uniform(0, 1)
-    # If less than probability, mutate
-    if rnd <= conf.pm:
-        # Generate a random property to alter
-        rnd = int(random.uniform(0, len(ItemTypes)))
-        # Get item for that index
-        item = ItemTypes(rnd)
-        # Generate random item
-        if item != ItemTypes.ALTURA:
-            newItem = items.getRandomItem(item)
-        else:
-            newItem = items.getRandomHeight()
-        # Set the gene in the character
-        ch.setGene(item, newItem)
+    def __mutacionLimitada(ch):
+        return ch
+
+    def __mutacionUniforme(ch):
+        return ch
+
+    # -----------------------------------------------------------------
+    # EXPOSED FUNCTIONS
+    # -----------------------------------------------------------------
+
+    # Exposed method to calculate the mutation
+    def apply(self, ch):
+        return self.mutacion(self, node, board)
+
+    # Map with pointers to the functions
+    mutacion = {
+        MutacionOptions.GEN: __mutacionGen, 
+        MutacionOptions.LIMITADA: __mutacionLimitada, 
+        MutacionOptions.UNIFORME: __mutacionUniforme,
+        MutacionOptions.COMPLETA: __mutacionCompleta
+    }
             
