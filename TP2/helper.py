de8a9708e2f4a5a47dcaeb3305042fd288daf4d6
Gonzalo Hirsch <hirschgonzalo@gmail.com>
2021-03-27 16:30:00 -0300

    Added real time graphics

--- TP2/helper.py
+++ TP2/helper.py
@@ -3,3 +3,11 @@ import numpy as np
 def getRelativeFitnesses(fitnesses):
     total = np.sum(fitnesses)
     return fitnesses/total
+
+# Calculates minimum fitness and the average fitness
+def getFitnessStats(chs):
+    fitnesses = np.array([])
+    # Store the fitness of the characters in an array
+    for ch in chs:
+        fitnesses = np.append(fitnesses, ch.fitness)
+    return np.amin(fitnesses), np.average(fitnesses)
--- TP2/main.py
+++ TP2/main.py
@@ -1,10 +1,58 @@
+# Lib imports
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import threading
+import os
 # Local imports
 from character import Character
 import inputParser as parser
 from geneticAlgorithm import GeneticAlgorithm
+from helper import getFitnessStats
 
 # Variables pointing to configs
 CONFIG_INPUT = "input/configuration.json"
+DATA_OUTPUT = "output/output.csv"
+OUTPUT_DIR = "output/"
+OUTPUT_FIELDNAMES = ["generacion", "minimo", "promedio"]
+
+# Function to run frame by frame
+def animate(i):
+    data = pd.read_csv(DATA_OUTPUT)
+    x = data[OUTPUT_FIELDNAMES[0]]
+    min = data[OUTPUT_FIELDNAMES[1]]
+    avg = data[OUTPUT_FIELDNAMES[2]]
+    plt.cla()
+    plt.plot(x, min, label='Fitness Mínima')
+    plt.plot(x, avg, label='Fitness Promedio')
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+
+# Function to run the animation
+def run_animation():
+    plt.style.use('fivethirtyeight')
+    ani = FuncAnimation(plt.gcf(), animate, interval=250)
+    plt.xlabel("Generación")
+    plt.tight_layout()
+    plt.show()
+
+# Makes sure the CSV file is prepared, create it if non existent
+def prepareOutput():
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+    with open(DATA_OUTPUT, 'w+') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=OUTPUT_FIELDNAMES)
+        csv_writer.writeheader()
+
+# Writes a row of data to the output file
+def writeRow(writer, gen, min, avg):
+    info = {
+        OUTPUT_FIELDNAMES[0]: gen,
+        OUTPUT_FIELDNAMES[1]: min,
+        OUTPUT_FIELDNAMES[2]: avg
+    }
+    writer.writerow(info)
 
 def main():
     # Parse configuration files
@@ -17,14 +65,21 @@ def main():
     # Generate the initial random population
     population = ga.generateInitialPopulation(config.n)
 
+    # Prepare output file
+    prepareOutput()
+
     generation = 0
-
-    for p in population:
-        print(p)
-
+        
     # Iterate while terminal condition is not met
     while not ga.isTerminated(population):
         print('GENERATION #' + str(generation))
+        # Calculate stats and write to file
+        min, average = getFitnessStats(population)
+        # Perform all with open file
+        with open(DATA_OUTPUT, 'a') as csv_file:
+            # Get instance of the writer
+            csv_writer = csv.DictWriter(csv_file, fieldnames=OUTPUT_FIELDNAMES)
+            writeRow(csv_writer, generation, min, average)
         # Select parents for next generation
         parents = ga.select(population, config.k, config.a)
         # Cross parents to generate children
@@ -41,4 +96,8 @@ def main():
     
 # App entrypoint
 if __name__ == "__main__":
-    main()
+    # Create the thread for the processing
+    x = threading.Thread(target=main, args=())
+    x.start()
+    # Run animation on main thread
+    run_animation()
