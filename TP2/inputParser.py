e03325a027995b5aa9269e90269866264f224a6f
Gonzalo Hirsch <hirschgonzalo@gmail.com>
2021-04-10 14:13:43 -0300

    Fixed old inputParser

--- TP2/inputParser.py
+++ TP2/inputParser.py
@@ -1,17 +1,52 @@
 # Lib imports
-
+import pandas as pd
+import os
+import json
 # Local imports
-
+from constants import ItemFiles, ConfigOptions
+from items import Items
+from config import Config
 
 # Function to parse all the items in the files
-def parseInput(filepath):
-    with open(filepath) as f:
-        # Read all lines
-        lines = f.readlines()
-        data = []
+def parseItems(directoryPath):
+    # Array to hold all the information
+    data = []
+    # Iterate and extract the files
+    for itemFile in ItemFiles:
+        # Parse file
+        df = pd.read_csv(os.path.join(directoryPath, itemFile.value), sep='\t', header=0, index_col=0)
+        # Push dataframe to array
+        data.append(df)
+    # Generate new items class
+    Items.setInstance(data)
 
-        for line in lines:
-            line = line.strip().split()
-            print(line)
-
-parseInput()
+# Parses the configuration
+def parseConfiguration(configPath):
+    with open(configPath) as json_file:
+        data = json.load(json_file)
+        # Get submaps inside config
+        gen = data[ConfigOptions.GEN.value]
+        nums = data[ConfigOptions.NUMS.value]
+        plot = data[ConfigOptions.PLOT.value]
+        # Create config
+        config = Config(
+            clase=data[ConfigOptions.CLASE.value], 
+            data=data[ConfigOptions.DATA.value], 
+            cruce=gen[ConfigOptions.CRUCE.value], 
+            mutacion=gen[ConfigOptions.MUTACION.value], 
+            seleccion=gen[ConfigOptions.SELECCION.value], 
+            reemplazo=gen[ConfigOptions.REEMPLAZO.value], 
+            implementacion=gen[ConfigOptions.IMPLEMENTACION.value], 
+            corte=gen[ConfigOptions.CORTE.value], 
+            a=nums[ConfigOptions.A.value], 
+            b=nums[ConfigOptions.B.value], 
+            n=nums[ConfigOptions.N.value], 
+            k=nums[ConfigOptions.K.value],
+            pm=nums[ConfigOptions.PM.value],
+            pcruce=nums[ConfigOptions.P_CRUCE.value],
+            crit1=nums[ConfigOptions.CRITERIO_1.value],
+            crit2=nums[ConfigOptions.CRITERIO_2.value],
+            show=plot[ConfigOptions.SHOW.value],
+            sampling=plot[ConfigOptions.SAMPLING.value]
+        )
+    return config
