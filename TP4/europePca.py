01492695fef6ace833674ea883273ce2e3262be1
Gonzalo Hirsch <hirschgonzalo@gmail.com>
2021-05-08 09:16:46 -0300

    Corrections to europePCA

--- TP4/europePca.py
+++ TP4/europePca.py
@@ -9,13 +9,9 @@ INPUT_FILE = 'datasets/europe.csv'
 # Extract and normalize data
 df = pd.read_csv(INPUT_FILE, sep=',', header=0, index_col=0)
 
-# Scale data due to large range differences
-minMaxScaler = preprocessing.MinMaxScaler()
-scaledDf = minMaxScaler.fit_transform(df)
-
 # Standarize data to get mean=0 and std=1
 standardScaler = preprocessing.StandardScaler()
-standarizedDf = standardScaler.fit_transform(scaledDf)
+standarizedDf = standardScaler.fit_transform(df)
 
 # Calculate components
 pca = PCA(n_components=2)
@@ -51,14 +47,11 @@ plt.show()
 # Biplot
 plt.style.use('seaborn-bright')
 fig, ax = plt.subplots(figsize=(12,7))
-# Calculate scaling factor for points
-scalex = 1.0/(valuesDf["PC1"].max() - valuesDf["PC1"].min())
-scaley = 1.0/(valuesDf["PC2"].max() - valuesDf["PC2"].min())
 # Create scatter
-ax.scatter(x=valuesDf["PC1"]*scalex, y=valuesDf["PC2"]*scaley)
+ax.scatter(x=valuesDf["PC1"], y=valuesDf["PC2"])
 # Add annotations
 for i, txt in enumerate(np.array(df.index)):
-    ax.annotate(txt, (valuesDf["PC1"][i]*scalex, valuesDf["PC2"][i]*scaley))
+    ax.annotate(txt, (valuesDf["PC1"][i], valuesDf["PC2"][i]))
 # Add feature vectors
 for vec in zip(componentsDf["PC1"].values, componentsDf["PC2"].values):
     plt.quiver(0, 0, vec[0], vec[1], angles='xy', scale_units='xy', scale=1, color='r', width=0.001)
@@ -69,12 +62,10 @@ for i, txt in enumerate(np.array(df.columns)):
 ax.set_xlabel("PC1 (%{})".format(round(pcaFit.explained_variance_ratio_[0]*100, 2)))
 ax.set_ylabel("PC2 (%{})".format(round(pcaFit.explained_variance_ratio_[1]*100, 2)))
 # Add internal gridlines
-major_ticks = np.arange(-0.6, 0.8, 0.1)
-minor_ticks = np.arange(-0.6, 0.8, 0.05)
-ax.set_xticks(major_ticks)
-ax.set_xticks(minor_ticks, minor=True)
-ax.set_yticks(major_ticks)
-ax.set_yticks(minor_ticks, minor=True)
+ax.set_xticks(np.arange(-4, 5, 0.5))
+ax.set_xticks(np.arange(-4, 5, 0.25), minor=True)
+ax.set_yticks(np.arange(-3, 4, 0.5))
+ax.set_yticks(np.arange(-3, 4, 0.25), minor=True)
 ax.grid(which='minor', alpha=0.25)
 ax.grid(which='major', alpha=0.5)
 ax.set_title("PCA for European Countries")
